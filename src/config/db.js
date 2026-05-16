ae074651b25b3305bfac8593dfb86d0ba6eb279c
Ken Eric <kene91446@gmail.com>
2026-05-16 20:33:32 +0400

    fix ssl connection for mongodb atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log('MongoDB connected');
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      tls: true,
+      tlsAllowInvalidCertificates: false,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
